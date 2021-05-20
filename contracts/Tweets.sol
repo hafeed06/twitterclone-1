f61cd62baa8b8bb0738a0410b7045cf3a5ebea59
Boris B <bamboriz1@gmail.com>
2021-05-20 02:59:07 +0200

    Feat: added readTweets func

--- contracts/Tweets.sol
+++ contracts/Tweets.sol
@@ -1,20 +1,27 @@
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.8.0;
 
-contract Tweets{
+contract Tweets {
 
-Tweeter[]  public tweets;
+Tweeter[] public tweets;
 
-struct Tweeter{
-    uint id;
-    string content;
-    address author;
-    bool deleted;
-}
+    struct Tweeter {
+        uint id;
+        string content;
+        address author;
+        bool deleted;
+    }
 
-function createTweets(string memory _content) public {
-     uint id = tweets.length +1;
-     tweets.push(Tweeter(id, _content, msg.sender, false));
-        
-}
+    /**
+	 * @dev We can fix the comment later. This is just a fix since we were having issues access the tweets array direclty
+     * Now this should work 'tweets = await instance.readTweets()'
+	 */
+    function readTweets() public view returns (Tweeter[] memory){
+        return tweets;
+    }
+
+    function createTweets(string memory _content) public {
+        uint id = tweets.length + 1;
+        tweets.push(Tweeter(id, _content, msg.sender, false));
+    }
 
 }
